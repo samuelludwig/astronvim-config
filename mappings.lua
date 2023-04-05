46860348ce6704fdb1e279a75bed051970164a2b
Samuel Ludwig <SLudwig.dev@gmail.com>
2023-04-05 17:22:57 -0400

    Add nice term escape and swap window nav+resizing

--- mappings.lua
+++ mappings.lua
@@ -22,10 +22,21 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    -- swapsies
+    ["<C-Left>"] = { function() require("smart-splits").move_cursor_left() end, desc = "Move to left split" },
+    ["<C-Down>"] = { function() require("smart-splits").move_cursor_down() end, desc = "Move to below split" },
+    ["<C-Up>"] = { function() require("smart-splits").move_cursor_up() end, desc = "Move to above split" },
+    ["<C-Right>"] = { function() require("smart-splits").move_cursor_right() end, desc = "Move to right split" },
+    ["<C-k>"] = { function() require("smart-splits").resize_up() end, desc = "Resize split up" },
+    ["<C-j>"] = { function() require("smart-splits").resize_down() end, desc = "Resize split down" },
+    ["<C-h>"] = { function() require("smart-splits").resize_left() end, desc = "Resize split left" },
+    ["<C-l>"] = { function() require("smart-splits").resize_right() end, desc = "Resize split right" },
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+    ["<esc>"] = { "<C-\\><C-n>", desc = "Back to normal mode" },
   },
   v = {},
   i = {},
